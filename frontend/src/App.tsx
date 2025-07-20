75a2bc51288d56ecb59323e764baf97dd5d306f1
Vincent Nilam <vincent.nilam@hotmail.com>
2025-07-20 16:50:07 +1000

    fix: fix media query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -189,7 +189,7 @@ const App: React.FC = () => {
             </button>
           </div>
 
-          <div className="grid gap-4 justify-center mb-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3">
+          <div className="grid gap-4 justify-center mb-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-2">
             {courts.map((court, i) => {
               const enrichedPlayers = court.players.map((p) => {
               const match = players.find((gp) => gp.name === p.name);
